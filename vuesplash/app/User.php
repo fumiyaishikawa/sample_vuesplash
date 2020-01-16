c472896f640af4290e6aadedd604cac87fe93eb8
fumiya ishikawa <programer.ishikawa@gmail.com>
2020-01-17 00:49:40 +0900

    写真投稿APIの作成

--- vuesplash/app/User.php
+++ vuesplash/app/User.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App;
-
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -27,4 +25,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * リレーションシップ - photosテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
 }
